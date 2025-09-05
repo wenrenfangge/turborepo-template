b87a4e92cec030ff9968ce4440685da70b946054
hujian <wenrenfangge@foxmail.com>
2025-09-05 18:07:24 +0800

    feat: 项目架构、开发自动将包转写入pnpm-workspace.yaml shell工具
    Signed-off-by: hujian <wenrenfangge@foxmail.com>

---
+++ packages/types/global.d.ts
@@ -0,0 +1,7 @@
+import type { RouteMeta as IRouteMeta } from "@repo-core/typings";
+import "vue-router";
+
+declare module "vue-router" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface RouteMeta extends IRouteMeta {}
+}
