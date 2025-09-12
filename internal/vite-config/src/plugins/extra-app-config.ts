7c4da4f28168fa6f945fa5af1689d27fd8b7d79b
hujian <wenrenfangge@foxmail.com>
2025-09-12 13:58:56 +0800

    perf: 配置优化
    Signed-off-by: hujian <wenrenfangge@foxmail.com>

--- internal/vite-config/src/plugins/extra-app-config.ts
+++ internal/vite-config/src/plugins/extra-app-config.ts
@@ -14,7 +14,7 @@ interface PluginOptions {
 }
 
 const GLOBAL_CONFIG_FILE_NAME = '_app.config.js';
-const repo_ADMIN_PRO_APP_CONF = '_repo_ADMIN_PRO_APP_CONF_';
+const REPO_APP_CONF = '_REPO_APP_CONF_';
 
 /**
  * 用于将配置文件抽离出来并注入到项目中
@@ -72,12 +72,12 @@ async function viteExtraAppConfigPlugin({
 
 async function getConfigSource() {
   const config = await loadEnv();
-  const windowVariable = `window.${repo_ADMIN_PRO_APP_CONF}`;
+  const windowVariable = `window.${REPO_APP_CONF}`;
   // 确保变量不会被修改
   let source = `${windowVariable}=${JSON.stringify(config)};`;
   source += `
     Object.freeze(${windowVariable});
-    Object.defineProperty(window, "${repo_ADMIN_PRO_APP_CONF}", {
+    Object.defineProperty(window, "${REPO_APP_CONF}", {
       configurable: false,
       writable: false,
     });
